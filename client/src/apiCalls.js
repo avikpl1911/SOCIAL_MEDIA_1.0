fce7bc8a2bccc62bd7e73d53b5ccd45bca082d26
nameoz <65647597+avikpl1911@users.noreply.github.com>
2023-01-25 14:22:52 +0530

    Update apiCalls.js

--- client/src/apiCalls.js
+++ client/src/apiCalls.js
@@ -6,7 +6,7 @@ import axios from "axios";
 export const loginCall = async (userCredential,dispatch) => {
    dispatch({type:"LOGIN_START"});
    try {
-    const res = await axios.post(process.env.REACT_APP_API_URL+"/api/auth/login",userCredential);
+    const res = await axios.post(process.env.REACT_APP_API_URL+"/auth/login",userCredential);
     dispatch({type:"LOGIN_SUCCESS",payload:res.data});
    } catch (err) {
     dispatch({type:"LOGIN_FAILURE",payload:err});
